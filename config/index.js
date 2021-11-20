a54ffb91df16552e095fd43db89fcf738821f90e
Diego <diegofajardo40@gmail.com>
2021-11-19 20:31:37 -0500

    Commit

--- config/index.js
+++ config/index.js
@@ -1,3 +1,3 @@
 const dev = process.env.NODE_ENV !== 'production'
 
-export const server = dev ? "http://localhost:3000" : 'https://dlezamaf-portfolio-uag7b.ondigitalocean.app/'
+export const server = dev ? "http://localhost:3000" : 'https://dlezamaf-portfolio.vercel.app'
